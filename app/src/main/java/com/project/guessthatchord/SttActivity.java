bb5c136055ad14068e016adcf5b71f432cd81cf5
ngynls <ngyn.ls@gmail.com>
2018-09-24 22:50:47 -0400

    Added an extra prompt in the Recognizer Intent

--- app/src/main/java/com/project/guessthatchord/SttActivity.java
+++ app/src/main/java/com/project/guessthatchord/SttActivity.java
@@ -75,6 +75,7 @@ public class SttActivity extends AppCompatActivity {
                 Intent intent= new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH);
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT,R.string.prompt);
                 try{
                     startActivityForResult(intent,200);
                 }
